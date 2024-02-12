eb7d8ff4a02ab731663071715a7e5db706360ccb
VitalyRK <shishovip@yandex.ru>
2024-02-12 10:01:15 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'simple-import-sort'],
+  rules: {
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^\\w', '^@hookform', '^@radix-ui'],
+              // npm packages
+              // Anything that starts with a letter (or digit or underscore), or `@` followed by a letter.
+              // ['^\\w'],
+              // Internal packages.
+              ['^@store(/.*|$)'],
+              ['^@components(/.*|$)'],
+              ['^@ui(/.*|$)'],
+              ['^@lib(/.*|$)'],
+              ['^@pages(/.*|$)'],
+              ['^@utils(/.*|$)'],
+              ['^@hooks(/.*|$)'],
+              ['^@services(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+}
